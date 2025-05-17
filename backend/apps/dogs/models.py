a2da4a34045ed8b4fc3d1049c1b950178aab2095
Corner324 <mosenn@mail.ru>
2025-05-18 00:52:06 +0300

    Добавление docstring

--- backend/apps/dogs/models.py
+++ backend/apps/dogs/models.py
@@ -3,21 +3,31 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Breed(models.Model):
+    """Модель породы собаки.
+
+    Атрибуты:
+        name (str): Название породы.
+        breedSize (str): Размер породы (Tiny, Small, Medium, Large).
+        friendliness (int): Уровень дружелюбия (1-5).
+        trainability (int): Уровень обучаемости (1-5).
+        shedding_amount (int): Интенсивность линьки (1-5).
+        exercise_needs (int): Потребность в активности (1-5).
+    """
 
     class BreedSize(models.TextChoices):
+        """Перечисление размеров породы."""
         TINY = "Tiny", "Tiny"
         SMALL = "Small", "Small"
         MEDIUM = "Medium", "Medium"
         LARGE = "Large", "Large"
 
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, verbose_name="Название породы")
     breedSize = models.CharField(
         max_length=7,
         choices=BreedSize.choices,
         default=BreedSize.MEDIUM,
         verbose_name="Размер",
     )
-
     friendliness = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)],
         verbose_name="Дружелюбие",
@@ -35,7 +45,7 @@ class Breed(models.Model):
         verbose_name="Потребность в активности",
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
     class Meta:
@@ -44,10 +54,22 @@ class Breed(models.Model):
 
 
 class Dog(models.Model):
+    """Модель собаки.
+
+    Атрибуты:
+        name (str): Имя собаки.
+        age (int): Возраст.
+        breed (Breed): Ссылка на модель породы.
+        gender (str): Пол (Самец, Самка).
+        color (str): Цвет шерсти.
+        favorite_food (str): Любимая еда.
+        favorite_toy (str): Любимая игрушка.
+    """
 
     class DogGender(models.TextChoices):
-        MALE = "самец", "самец"
-        FEMALE = "самка", "самка"
+        """Перечисление полов собаки."""
+        MALE = "Самец", "Самец"
+        FEMALE = "Самка", "Самка"
 
     name = models.CharField(max_length=20, verbose_name="Имя")
     age = models.IntegerField(verbose_name="Возраст")
@@ -62,7 +84,7 @@ class Dog(models.Model):
     favorite_food = models.CharField(max_length=20, verbose_name="Любимая еда")
     favorite_toy = models.CharField(max_length=20, verbose_name="Любимая игрушка")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
     class Meta:
